f3109f36844f711a00c6c57f51fa18966a65de38
pr0vin <pravinchaudhary688@gmail.com>
2025-12-10 21:58:47 +0545

    datas

---
+++ resources/js/data.js
@@ -0,0 +1,39 @@
+const stats = [
+    { count: 25, label: "Years of Excellence" },
+    { count: 5, label: "Programs" },
+    { count: 302, label: "UG Intake / Year" },
+    { count: 70, label: "Grad Intake / Half Year" },
+    { count: 33, label: "Faculties" },
+];
+
+const programmes = [
+    {
+        tag: "BE Computer",
+        title: "Computer Engineering",
+        img: "/img/computer.jpg",
+    },
+    { tag: "BCA", title: "Computer Application", img: "/img/bca.jpeg" },
+    { tag: "BBA", title: "Business Administration", img: "/img/bba.jpg" },
+    { tag: "BE Civil", title: "Civil Engineering", img: "/img/civil.jpg" },
+    { tag: "MBA", title: "Master's Business Admin", img: "/img/mba.jpg" },
+];
+
+const whyCards = [
+    {
+        img: "https://cdn-icons-png.flaticon.com/512/3135/3135715.png",
+        title: "Exploring Potential",
+        text: "Discovering talents in western region.",
+    },
+    {
+        img: "https://cdn-icons-png.flaticon.com/512/1828/1828884.png",
+        title: "Affordable & Quality",
+        text: "Top quality education within reach.",
+    },
+    {
+        img: "https://cdn-icons-png.flaticon.com/512/9068/9068979.png",
+        title: "Institutional Collaboration",
+        text: "Collaborating with leading institutions.",
+    },
+];
+
+export { stats, programmes, whyCards };
